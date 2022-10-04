a86482924d2ddd052c4c3d003966144b8d0e4728
miura3413413 <114330852+miura3413413@users.noreply.github.com>
2022-10-04 16:59:20 +0900

    add dummy

---
+++ src/models/dummydata.js
@@ -0,0 +1,32 @@
+export const Tweet = [
+  {
+  text: "test1",
+  reply: 0,
+  retweet: 0,
+  like: 0, 
+  },
+  {
+    text: "test2",
+    reply: 1,
+    retweet: 1,
+    like: 1, 
+  },
+  {
+    text: "test3",
+    reply: 10,
+    retweet: 10,
+    like: 10, 
+  },
+  {
+    text: "test4",
+    reply: 100,
+    retweet: 100,
+    like: 100, 
+  },
+  {
+    text: "test5",
+    reply: 1000,
+    retweet: 1000,
+    like: 1000, 
+  },
+]
