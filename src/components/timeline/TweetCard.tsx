d3e1b961fdb2ac38231a7928c9adb6518cc031d5
miura3413413 <114330852+miura3413413@users.noreply.github.com>
2022-10-23 00:31:16 +0900

    changed name

---
+++ src/components/timeline/TweetCard.tsx
@@ -0,0 +1,107 @@
+import { NextPage } from "next"
+import { ProfileIcon } from "../icon/ProfileIcon"
+import { IconText } from "../icon/IconText";
+import styled from '@emotion/styled'
+
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import AutorenewIcon from '@mui/icons-material/Autorenew';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import { dummyTweetType } from "../../models/dummydata";
+import { useState } from "react";
+import { TweetType } from "../../models/TweetModel";
+import { IconButton } from "@mui/material";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { IconNumber } from "../icon/IconNumber";
+
+const StyledDiv = styled.div`
+  width: 100%;
+  height: auto;
+  border-bottom: solid thin #daeeee;
+  border-right: solid thin #daeeee;
+  display: flex;
+  :hover{
+    background-color: #efefef;
+  }
+`
+
+const StyledDivRight = styled.div`
+  width: 80%;
+  margin: 0;
+`
+
+const StyledDivTop = styled.div`
+  margin: 15px 0px 0 0;
+  display: flex;
+  
+`
+
+const StyledSpanName = styled.span`
+  margin: 0;
+  font-weight: bold;
+`
+
+const StyledSpanTime = styled.span`
+  margin: 0 0 0 5px;
+`
+
+const StyledSpanTweet = styled.span`
+  margin-right: 20px;
+  
+  overflow-wrap: break-word;
+`
+
+export const TweetCard : NextPage<{tweet: TweetType}> = ({tweet}) => {
+const router = useRouter()
+const [retweet ,setRetweet] = useState(tweet.retweet)
+const [isRetweet ,setIsRetweet] = useState(false)
+const [like ,setLike] = useState(tweet.like)
+const [isLike ,setIsLike] = useState(false)
+const clickRetweet = (e: React.MouseEvent<HTMLInputElement>) => {
+  e.stopPropagation()
+  setRetweet(isRetweet? retweet -1 : retweet +1)
+  setIsRetweet(!isRetweet)
+}
+
+const clickLike = (e: React.MouseEvent<HTMLInputElement>) => {
+  e.stopPropagation()
+  setLike(isLike? like -1 : like +1)
+  setIsLike(!isLike)
+}
+
+const routeHandler = () => {
+  router.push('/test')
+}
+
+//const clickDelete = async() => { {
+//   try {
+//     await axios.delete("/api/test/tweet")
+//     router.reload()
+//   } catch(err) {
+//     console.log(err)
+//   }
+//}
+  return (
+    <StyledDiv onClick={routeHandler}>
+      <ProfileIcon />
+      <StyledDivRight>
+        <StyledDivTop >
+          <StyledSpanName>名前</StyledSpanName>
+          <StyledSpanTime>時間</StyledSpanTime>
+          <IconButton aria-label="delete"  sx={{margin: "auto 0px auto auto"}}>
+            <DeleteForeverIcon />
+          </IconButton>
+        </StyledDivTop>
+        <StyledSpanTweet>
+          {tweet.text}
+        </StyledSpanTweet>
+        <div style ={{display : "flex", alignItems: "center", width: "20%"}}>
+          <IconNumber Icon={ChatBubbleOutlineIcon} number={tweet.reply} />
+          <IconNumber Icon={AutorenewIcon} number={retweet} stateFunction={clickRetweet}/>
+          <IconNumber Icon={FavoriteBorderIcon} number={like} stateFunction={clickLike} />
+        </div>
+      </StyledDivRight>
+    </StyledDiv>
+  )
+}
