0260c91bfcfa225189259583f56bec7c20d325b3
miura3413413 <114330852+miura3413413@users.noreply.github.com>
2022-10-24 16:33:48 +0900

    css

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,9 +2,15 @@ import type { NextPage } from 'next'
 import Image from 'next/image'
 import styled from '@emotion/styled';
 
+interface Props {
+  color?: string
+  bgColor?: string
+  hoverBgColor: string
+}
+
 const StyledDiv = styled.div`
   display: flex;
-  height: 100vh;
+  height: 100%;
 `
 const StyledDivLeft = styled.div`
   width: auto;
@@ -12,6 +18,7 @@ const StyledDivLeft = styled.div`
   position: relative;
   @media(max-width: 1000px) {
     display: none;
+
   };
 `
 
@@ -28,22 +35,66 @@ const StyledDivRight = styled.div`
   width: 900px;
   @media(max-width: 1000px) {
     width: 100%;
+
+
   };
 ` 
 
 const StyledDivMain = styled.div`
-  height: 83vh;
-  padding-top: 50px;
-  padding-left: 30px;
+  height: 750px;
+  padding: 50px 0 0 30px;
+  @media(max-width: 1000px) {
+    padding-top: 30px;
+  };
 `
 
 const StyledH1 = styled.h1`
   font-size: 60px;
+  @media(max-width: 1000px) {
+    margin: 10px 0;
+  };
 `
+
+
+
+const StyledDivButton = styled.div`
+  display:flex;
+  flex-flow: column;
+`
+
+const StyledButton = styled.button<Props>`
+  width: 300px;
+  height: 40px;
+  background-color: ${({bgColor}) => (bgColor || "white")} ;
+  color: ${({color}) => (color || "black")};
+  font-weight: bold;
+  border: 1px solid #e0e0e0;
+  border-radius: 40px;
+  margin: 5px;
+  cursor: pointer;
+  transition: 0.3s;
+  :hover{
+    background-color: ${({hoverBgColor}) => hoverBgColor};
+  }
+`
+
+const StyledP = styled.p`
+  margin: 5px;
+  padding-left: 130px;
+  font-weight: bold;
+  font-size: smaller;
+`
+
+const StyledH4 = styled.h4`
+  margin: 0;
+  padding: 100px 0 10px 0;
+
+`
+
 
 const StyledDivButtomImage = styled.div`
   width: 100%;
-  height: 17vh;
+  height: 170px;
   position: relative;
   
   @media(min-width: 1000px) {
@@ -68,15 +119,17 @@ export const Login: NextPage = () => {
       </StyledDivLeft>
       <StyledDivRight>
         <StyledDivMain>
-          <Image src="/2021 Twitter logo - blue.png" width={50} height={50} objectFit="contain"/>
+          <Image src="/2021 Twitter logo - blue.png" width={40} height={40} objectFit="contain"/>
           <StyledH1>すべての話題が、ここに。</StyledH1>
-          <h2>Twitterを始めよう</h2>
-          <button>Googleで登録</button>
-          <button>Appleのアカウントで登録</button>
-          <p>または</p>
-          <button>電話番号またはメールアドレスで登録</button>
-          <h3>アカウントをお持ちの場合</h3>
-          <button>ログイン</button>
+          <h2>Twitterをはじめよう</h2>
+          <StyledDivButton>
+            <StyledButton hoverBgColor='#eef2fc'>Googleで登録</StyledButton>
+            <StyledButton hoverBgColor='#e2e2e2'>Appleのアカウントで登録</StyledButton>
+          </StyledDivButton>
+          <StyledP>または</StyledP>
+          <StyledButton bgColor='#4aa6e7' color='white' hoverBgColor='#308ce3'>電話番号またはメールアドレスで登録</StyledButton>
+          <StyledH4>アカウントをお持ちの場合</StyledH4>
+          <StyledButton color='#1a82d7' hoverBgColor='#eaf2fe'>ログイン</StyledButton>
         </StyledDivMain>
         <StyledDivButtomImage>
           <Image src="/lohp_1302x955.png"  layout="fill"  objectFit="cover"/>
