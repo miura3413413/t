a9c7e11b1c76cd775cc872a34eb135686437a915
miura3413413 <114330852+miura3413413@users.noreply.github.com>
2022-11-01 04:14:29 +0900

    home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,22 @@
 import type {  NextPage } from 'next'
 import { Timeline } from '../components/timeline/Timeline'
 import Layout from '../components/layout/Layout'
+import { useEffect } from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 
   export const Home: NextPage = () => {
+    const { data: session} = useSession()
+    const router = useRouter()
+    useEffect(() => {
+      if(session?.user) {
+        router.push("/home")
+      }else{
+        router.push("/")
+      }
+      
+    }, [session])
+    
     return (
       <Layout title={"ホーム"}>
       <Timeline/>
